edfe8a1fed10497d69e28914ed079502d3387b90
Masha <nasonkina@yandex.ru>
2023-12-14 19:32:40 +0300

    fix 485 solution

--- 485.js
+++ 485.js
@@ -5,13 +5,17 @@
 const findMaxConsecutiveOnes = (nums) => {
   let curSum = 0;
   let maxSum = 0;
-  for (let i = 0; i < nums.length; i += 1) {
-    if (nums[i] === 1) {
+
+  for (const n of nums) {
+    if (n === 1) {
       curSum += 1;
-      maxSum = Math.max(curSum, maxSum);
     } else {
+      maxSum = Math.max(maxSum, curSum);
       curSum = 0;
     }
   }
+
+  maxSum = Math.max(maxSum, curSum);
+
   return maxSum;
 };
