54dadcbdb0c3330e071a1627ecb4f391619fa166
Masha <nasonkina@yandex.ru>
2024-04-28 23:21:46 +0300

    fix 977

--- 977.js
+++ 977.js
@@ -3,6 +3,22 @@
  * @return {number[]}
  */
 const sortedSquares = (nums) => {
+  // const result = [];
+  //   let left = 0;
+  //   let right = nums.length - 1;
+
+  //   while (left <= right) {
+  //       if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+  //           result.push(nums[left] ** 2)
+  //           left += 1;
+  //       } else {
+  //           result.push(nums[right] ** 2)
+  //           right -= 1;
+  //       }
+  //   }
+
+  //   return result.reverse();
+
   const result = [];
   const stack = [];
 
