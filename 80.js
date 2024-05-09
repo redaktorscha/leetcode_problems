4bce78e3d6e404383668b727013ff1d166b8bc58
Masha <nasonkina@yandex.ru>
2024-05-09 16:00:47 +0300

    fix 80

--- 80.js
+++ 80.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 const removeDuplicates = (nums) => {
-  let k = 0;
   let count = 1;
+  let k = 0;
 
   for (let i = 1; i < nums.length; i += 1) {
-    if (nums[i] !== nums[i - 1]) {
+    if (nums[i] === nums[i - 1]) {
+      count += 1;
+    } else {
+      count = 1;
+    }
+
+    if (count <= 2) {
       k += 1;
       nums[k] = nums[i];
-      count = 1;
-    } else {
-      count += 1;
-      if (count <= 2) {
-        k += 1;
-        nums[k] = nums[i];
-      }
     }
   }
   return k + 1;
