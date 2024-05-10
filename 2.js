47f7e3d0d8896623115f0968771767be2437b8b4
Masha <nasonkina@yandex.ru>
2024-05-10 15:32:45 +0300

    fix 2

--- 2.js
+++ 2.js
@@ -16,32 +16,29 @@ const addTwoNumbers = (l1, l2) => {
   let carry = 0;
 
   const list = {
-      val: null,
-      next: null,
+    val: null,
+    next: null,
   };
 
   let current = list;
 
   while (node1 || node2 || carry) {
-      let sum = (node1?.val ?? 0) + (node2?.val ?? 0) + carry;
+    let sum = (node1?.val ?? 0) + (node2?.val ?? 0) + carry;
 
-      carry = Math.floor(sum / 10);
-      sum %= 10;
+    carry = Math.floor(sum / 10);
+    sum %= 10;
 
-      node1 = node1?.next ?? null;
-      node2 = node2?.next ?? null;
+    node1 = node1?.next ?? null;
+    node2 = node2?.next ?? null;
 
-      current.next = {
-          val: sum,
-          next: null,
-      };
-      current = current.next;
+    current.next = {
+      val: sum,
+      next: null,
+    };
+    current = current.next;
   }
 
   return list.next;
-  }
-
-  return JSON.stringify(list.next);
 };
 
 // const list2 = {
