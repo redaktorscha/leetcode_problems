e73122429c61569a00d7447fdbeb7ca0d5c91ae1
Masha <nasonkina@yandex.ru>
2024-05-13 20:04:13 +0300

    fix 5

--- 5.js
+++ 5.js
@@ -3,49 +3,73 @@
  * @return {string}
  */
 const longestPalindrome = (s) => {
-  const modifyStr = (str) => `#${str.split('').join('#')}#`;
+  // const modifyStr = (str) => `#${str.split('').join('#')}#`;
 
-  const modifiedStr = modifyStr(s);
+  // const modifiedStr = modifyStr(s);
 
-  const strLen = modifiedStr.length;
-  const pArr = [];
+  // const strLen = modifiedStr.length;
+  // const pArr = [];
 
-  let maxLen = 0;
-  let maxLeft = 0;
-  let maxRight = 0;
-  let centerIdx = 0;
-  let rightIdx = 0;
+  // let maxLen = 0;
+  // let maxLeft = 0;
+  // let maxRight = 0;
+  // let centerIdx = 0;
+  // let rightIdx = 0;
 
-  for (let i = 0; i < strLen; i += 1) {
-    const mirrorIdx = 2 * centerIdx - i;
+  // for (let i = 0; i < strLen; i += 1) {
+  //   const mirrorIdx = 2 * centerIdx - i;
 
-    if (i < rightIdx) {
-      pArr[i] = Math.min(pArr[mirrorIdx], rightIdx - i);
-    } else {
-      pArr[i] = 0;
-    }
+  //   if (i < rightIdx) {
+  //     pArr[i] = Math.min(pArr[mirrorIdx], rightIdx - i);
+  //   } else {
+  //     pArr[i] = 0;
+  //   }
 
-    let a = i + (pArr[i] + 1);
-    let b = i - (pArr[i] + 1);
+  //   let a = i + (pArr[i] + 1);
+  //   let b = i - (pArr[i] + 1);
 
-    while (a < strLen && b >= 0 && modifiedStr[a] === modifiedStr[b]) {
-      pArr[i] += 1;
-      a += 1;
-      b -= 1;
-    }
+  //   while (a < strLen && b >= 0 && modifiedStr[a] === modifiedStr[b]) {
+  //     pArr[i] += 1;
+  //     a += 1;
+  //     b -= 1;
+  //   }
 
-    if (i + pArr[i] > rightIdx) {
-      rightIdx = i + pArr[i];
-      centerIdx = i;
+  //   if (i + pArr[i] > rightIdx) {
+  //     rightIdx = i + pArr[i];
+  //     centerIdx = i;
 
-      if (pArr[i] > maxLen) {
-        maxLen = pArr[i];
+  //     if (pArr[i] > maxLen) {
+  //       maxLen = pArr[i];
 
-        maxLeft = i - pArr[i];
-        maxRight = i + pArr[i];
+  //       maxLeft = i - pArr[i];
+  //       maxRight = i + pArr[i];
+  //     }
+  //   }
+  // }
+
+  // return modifiedStr.slice(maxLeft, maxRight).split('#').join('');
+
+  //v.2
+
+  let maxLength = 0;
+  let result = '';
+
+  const checkPalindromeLength = (left, right) => {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentLength = right - left + 1;
+      if (currentLength > maxLength) {
+        maxLength = currentLength;
+        result = s.slice(left, right + 1);
       }
+      left -= 1;
+      right += 1;
     }
+  };
+
+  for (let i = 0; i < s.length; i += 1) {
+    checkPalindromeLength(i, i);
+    checkPalindromeLength(i, i + 1);
   }
 
-  return modifiedStr.slice(maxLeft, maxRight).split('#').join('');
+  return result;
 };
