feee8964a24d63e6e8429dac29b4ce728464d27c
Masha <nasonkina@yandex.ru>
2024-05-16 16:09:17 +0300

    fix 155

--- 155.js
+++ 155.js
@@ -1,55 +1,42 @@
-const MinStack = function () {
-  this.minStack = [];
-  this.orderedStack = [];
-};
+class MinStack {
+  constructor() {
+    this.min = [];
+    this.ordered = [];
+  }
 
-/**
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function (val) {
-  this.orderedStack.push(val);
-  const prevMin = this.minStack[this.minStack.length - 1];
-  this.minStack.push(val);
-
-  if (val > prevMin) {
-    let i = this.minStack.length - 1;
-
-    while (i > 0 && this.minStack[i - 1] < val) {
-      this.minStack[i] = this.minStack[i - 1];
-      i -= 1;
+  /**
+   * @param {number} val
+   * @return {void}
+   */
+  push(val) {
+    this.ordered.push(val);
+    if (this.min.length === 0) {
+      this.min.push(val);
+      return;
     }
 
-    this.minStack[i] = val;
+    this.min.push(Math.min(val, this.min[this.min.length - 1]));
   }
-};
 
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function () {
-  const deletedVal = this.orderedStack.pop();
-  let delIndex = -1;
-  for (let i = this.minStack.length - 1; i >= 0; i -= 1) {
-    if (this.minStack[i] === deletedVal && delIndex === -1) {
-      delIndex = i;
-    }
+  /**
+   * @return {void}
+   */
+  pop() {
+    this.ordered.pop();
+    this.min.pop();
   }
-  if (delIndex !== -1) {
-    this.minStack.splice(delIndex, 1);
+
+  /**
+   * @return {number}
+   */
+  top() {
+    return this.ordered[this.ordered.length - 1];
   }
-};
 
-/**
- * @return {number}
- */
-MinStack.prototype.top = function () {
-  return this.orderedStack[this.orderedStack.length - 1];
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
-};
+  /**
+   * @return {number}
+   */
+  getMin() {
+    return this.min[this.min.length - 1];
+  }
+}
